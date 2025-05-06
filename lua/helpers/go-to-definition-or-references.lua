9e685505f350d3187e17fa6a51820b46257fd805
Lukas Reiss <alukreis@gmail.com>
2025-05-06 14:51:55 +0100

    go to definition or references now defaults to definition if no tokens are under the cursor

--- lua/helpers/go-to-definition-or-references.lua
+++ lua/helpers/go-to-definition-or-references.lua
@@ -1,13 +1,16 @@
+local function is_declaration_or_definition(token)
+  local modifiers = token.modifiers
+  return modifiers.declaration or modifiers.definition
+end
+
 local function go_to_definition_or_references()
   local tokens = vim.lsp.semantic_tokens.get_at_pos()
   if tokens ~= nil then
     local firstToken = tokens[1]
-    if firstToken ~= nil then
-      if firstToken.modifiers.declaration or firstToken.modifiers.definition then
-        vim.lsp.buf.references()
-      else
-        vim.lsp.buf.definition()
-      end
+    if firstToken ~= nil and is_declaration_or_definition(firstToken) then
+      vim.lsp.buf.references()
+    else
+      vim.lsp.buf.definition()
     end
   end
 end
