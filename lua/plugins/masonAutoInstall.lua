99fe01cc1397a0ea8b98bcee03ee231f902fdad2
Lukas Reiss <alukreis@gmail.com>
2026-03-21 13:33:22 +0000

    added mason auto installer

---
+++ lua/plugins/masonAutoInstall.lua
@@ -0,0 +1,22 @@
+return {
+  'owallb/mason-auto-install.nvim',
+  dependencies = {
+    'mason-org/mason.nvim',
+    'neovim/nvim-lspconfig',
+  },
+  opts = {
+    packages = {
+      "black",
+      "stylua",
+      "eslint_d",
+      "prettierd",
+      "rust-analyzer",
+      "python-lsp-server",
+      "lua-language-server",
+      "bash-language-server",
+      "angular-language-server",
+      "typescript-language-server",
+      "tailwindcss-language-server",
+    }
+  },
+}
